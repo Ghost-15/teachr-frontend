ca4239712ee5047cd80ecfa2e99156eb19f3ff17
TimosLeBoss <tatibatchi15@hotmail.com>
2025-02-23 19:40:34 +0100

    Bien joue negro

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const BASE_URL = "http://localhost:8000/";
-const BASE_URL = "https://teachr-backend-3fqh.onrender.com";
+const BASE_URL = "http://localhost:8000/";
+// const BASE_URL = "https://teachr-backend-3fqh.onrender.com";
 
 export default axios.create({
     baseURL: BASE_URL
